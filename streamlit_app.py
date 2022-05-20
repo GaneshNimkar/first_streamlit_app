dc47227131639ace4326a852d274a670352c2ba3
Ganesh Nimkar <37946609+GaneshNimkar@users.noreply.github.com>
2022-05-20 03:35:14 +0100

    Update streamlit_app.py
    added header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title("My Parents new Healty Dinner")
+streamlit.title("My Moms Healty Dinner")
 streamlit.header("Breakfast favorits")
 streamlit.text("🥣 Omega 3 blueberry")
 streamlit.text(" 🥗 Kale,Spanish,Rocket Smoothie")
 streamlit.text(" 🐔 Eggs boiled")
 streamlit.text("🥑🍞 Avacado tost")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
