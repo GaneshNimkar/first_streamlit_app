c8837870e47a9a925be81d54332317d44e6fe965
Ganesh Nimkar <37946609+GaneshNimkar@users.noreply.github.com>
2022-05-20 03:22:16 +0100

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents new Healty Dinner")
+streamlit.header("Breakfast menue")
+streamlit.text("Omega 3 blueberry")
+streamlit.text("Kale,Spanish,Rocket Smoothie")
+streamlit.text("Ehhgs boiled")
