7c77b6de0cab6bad7baaee514c05c3c724c973b6
Ganesh Nimkar <37946609+GaneshNimkar@users.noreply.github.com>
2023-01-18 21:45:25 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,10 +17,7 @@ fruit_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header("Fruityvice Fruit Advice!")
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
 
 # Normalize JSON 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -29,6 +26,12 @@ streamlit.dataframe(fruityvice_normalized)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+streamlit.header("Fruityvice Fruit Advice!")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+streamlit.text(fruityvice_response.json())
+
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
