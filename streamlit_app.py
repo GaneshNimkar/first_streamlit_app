708cf4494e1be33e6c371c5613bd196f0ad0638b
Ganesh Nimkar <37946609+GaneshNimkar@users.noreply.github.com>
2023-01-18 22:06:02 +0000

    Update streamlit_app.py
    Exception handeled

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,17 +18,20 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruit_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruits_to_show)
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# Normalize JSON 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Create dataframe
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select the fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # Normalize JSON 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # Create dataframe
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  
+  streamlit.error();
 
 streamlit.stop()
 
